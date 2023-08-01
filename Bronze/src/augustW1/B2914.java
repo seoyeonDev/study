e10b69302e170cc4af5e2e74bb0c7ed07c1a2bf9
seoyeonDev <skylarlee1003@gmail.com>
2023-08-01 09:02:58 +0900

    230801 B3-2914

---
+++ Bronze/src/augustW1/B2914.java
@@ -0,0 +1,21 @@
+package augustW1;
+
+import java.util.Scanner;
+
+public class B2914 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//A:수록된 곡의 개수 I: 평균값, 
+		int A, I, All;
+		Scanner sc = new Scanner (System.in);
+		A = sc.nextInt();
+		I = sc.nextInt();
+		
+		All = A * (I-1)+1;
+		System.out.println(All);
+				
+	}
+
+}
