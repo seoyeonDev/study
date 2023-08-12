25f2b93bf5ba2cb9b141ae7257689848e13ac8f2
seoyeonDev <skylarlee1003@gmail.com>
2023-08-13 00:15:19 +0900

    230812

---
+++ Bronze/src/augustW2/B2446.java
@@ -0,0 +1,23 @@
+package augustW2;
+
+import java.util.Scanner;
+
+public class B2446 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int k;
+
+		for (int i = 0; i < N; i++) {
+			for(k = 0; k <  N ; k++) {
+				System.out.print("a");
+			}
+//			for (int j = 0; j <i - k; j++) {
+//				System.out.print("*");
+//			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ Silver/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author iseoyeon
+ *
+ */
+module Silver {
+}
