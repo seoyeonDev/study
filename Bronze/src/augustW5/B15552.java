cf982beade7658f671349dfda9e23c510aa094f3
seoyeonDev <skylarlee1003@gmail.com>
2023-08-28 23:37:35 +0900

    0828

--- Bronze/src/augustW5/B15552.java
+++ Bronze/src/augustW5/B15552.java
@@ -1,15 +1,27 @@
 package augustW5;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class B15552 {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) throws IOException{
 
-		Scanner sc = new Scanner (System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
+		int A = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<A; i++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+"\n"));
+		}
+		bw.close();
 		
 	}
 
