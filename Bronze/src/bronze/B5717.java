2470c3dcf17226ab6533538236be8c0a2d7ed78d
sylee <skylarlee1003@gmail.com>
2024-05-30 12:46:28 +0900

    240530

---
+++ Bronze/src/bronze/B5717.java
@@ -0,0 +1,21 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B5717 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            int M = sc.nextInt();
+            int F = sc.nextInt();
+
+            if (( M == 0 && F == 0 )|| 1 > M || F > 5){
+                break;
+            }
+            System.out.println(M + F);
+
+        }
+    }
+}
