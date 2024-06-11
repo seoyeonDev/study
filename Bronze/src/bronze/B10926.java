2725d35f2ef1519d936448a081ad05936a1919d7
seoyeonDev <skylarlee1003@gmail.com>
2024-06-11 14:18:17 +0900

    240611

---
+++ Bronze/src/bronze/B10926.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+
+        System.out.println(id + "??!");
+    }
+}
