dee2d594f4267b4694a20ef175a65c4bf76bea6b
seoyeonDev <skylarlee1003@gmail.com>
2024-06-16 23:30:52 +0900

    폴더 정리

---
+++ Study/src/bronze/B10818.java
@@ -0,0 +1,26 @@
+package Study.src.bronze;
+
+import java.util.Scanner;
+
+public class B10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0; i < N ; i++){
+            array[i] = sc.nextInt();
+        }
+        int max = array[0];
+        int min = array[0];
+        for(int num : array){
+            if(num > max){
+                max = num;
+            }
+            if (num < min){
+                min = num;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
