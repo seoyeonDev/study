b8fab0c33405d68da33b5a3056887e9e70464075
seoyeonDev <skylarlee1003@gmail.com>
2025-01-02 00:20:09 +0900

    0102

---
+++ Study/src/bronze/B25304.java
@@ -0,0 +1,29 @@
+package Study.src.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int total = 0;
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < b; i ++ ){
+            String c = br.readLine();
+            st = new StringTokenizer(c) ;
+
+            int price = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+
+            total += price * cnt;
+        }
+
+        System.out.println((total == a) ? "Yes" : "No");
+    }
+}
