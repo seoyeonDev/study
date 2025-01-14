6b8ddba668fa43b25e20a6a303d1439ff69c78b7
seoyeonDev <skylarlee1003@gmail.com>
2025-01-14 17:49:55 +0900

    0114

--- Study/src/bronze/B9086.java
+++ Study/src/bronze/B9086.java
@@ -1,7 +1,21 @@
 package Study.src.bronze;
 
-public class B9086 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class B9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        if (1 > T || T > 10) return;
+
+        for (int i = 0; i<T; i++){
+            String a = br.readLine();
+            if (a.length() > 1000) return;
+            System.out.print(a.charAt(0));
+            System.out.print(a.charAt(a.length() - 1));
+            System.out.println();
+        }
     }
 }
