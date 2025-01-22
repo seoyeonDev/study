db3854fcb86b3e8adbab98d4c4799e1f607cd1d8
seoyeonDev <skylarlee1003@gmail.com>
2025-01-22 13:52:05 +0900

    0121

--- Study/src/bronze/B1157_2.java
+++ Study/src/bronze/B1157_2.java
@@ -13,21 +13,25 @@ public class B1157_2 {
         String a = A.toUpperCase();
         if (a.length() > 1000000) return;
 
+        // 알파벳 개수
         int[] chk = new int[26];
         int max = 0;
         int maxIndex = 0;
 
+        // 각 알파벳이 사용된 횟수 기록
         for (int i = 0; i < a.length(); i++) {
             chk[a.charAt(i) - 'A']++;
         }
+
+        // 기록된 횟수로 max 값 구하기
         for (int i = 0; i < chk.length; i++) {
             if (max < chk[i]) {
                 max = chk[i];
                 maxIndex = i;
-            } else if (max == chk[i]) {
-                maxIndex = -2;
+            } else if (max == chk[i]) { // max 값과 동일한 값이 있을 경우 확인
+                maxIndex = -2;  // ? 출력
             }
         }
-        System.out.printf("%s", Character.toString(maxIndex + 'A'));
+        System.out.println(Character.toString(maxIndex + 'A'));
     }
 }
--- Study/src/bronze/B1316.java
+++ Study/src/bronze/B1316.java
@@ -1,7 +1,45 @@
 package Study.src.bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class B1316 {
-    public static void main(String[] args) {
+
+    private static boolean Chk(String a) {
+        boolean[] booleans = new boolean[26];
+        boolean rtn = true;
+
+        for (int i=0; i<a.length(); i++){
+            boolean aBoolean = booleans[a.charAt(i) - 'a'];
+            // 이미 기존 값이 true && 이전 글자와 다를 경우
+            if (aBoolean && a.charAt(i -1) != a.charAt(i)){
+                rtn = false;
+            }
+            booleans[a.charAt(i) - 'a'] = true;
+
+        }
+        return rtn;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N > 100 || 1 > N) return;
+        int cnt =0;
+
+        for (int i = 0; i < N; i++) {
+            String a = br.readLine();
+            if (a.length() > 100 || a.isEmpty()) return ;
+            if (Chk(a)){
+                cnt ++;
+            }
+        }
+        if (cnt > N) return;
+
+        System.out.println(cnt);
 
     }
 }
