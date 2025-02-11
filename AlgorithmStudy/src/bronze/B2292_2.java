e6361700250b1600dfac91bdeb6cf83742a892d1
seoyeonDev <skylarlee1003@gmail.com>
2025-02-11 14:54:08 +0900

    0211

--- AlgorithmStudy/src/bronze/B2292_2.java
+++ AlgorithmStudy/src/bronze/B2292_2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B2292 {
+public class B2292_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
